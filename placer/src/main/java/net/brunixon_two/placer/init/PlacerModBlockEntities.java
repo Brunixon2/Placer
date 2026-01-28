6d6d2e186b2e8824624e9b3cbd56665bb5b76d0e
Brunixon2 <brunixon2@gmail.com>
2026-01-28 16:42:50 +0100

    Update PlacerModBlockEntities.java

--- placer/src/main/java/net/brunixon_two/placer/init/PlacerModBlockEntities.java
+++ placer/src/main/java/net/brunixon_two/placer/init/PlacerModBlockEntities.java
@@ -1,6 +1,4 @@
-/*
- *    MCreator note: This file will be REGENERATED on each build.
- */
+
 package net.brunixon_two.placer.init;
 
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -24,8 +22,7 @@ public class PlacerModBlockEntities {
 	public static final DeferredRegister<BlockEntityType<?>> REGISTRY = DeferredRegister.create(BuiltInRegistries.BLOCK_ENTITY_TYPE, PlacerMod.MODID);
 	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<PlacerBlockEntity>> PLACER = register("placer", PlacerModBlocks.PLACER, PlacerBlockEntity::new);
 
-	// Start of user code block custom block entities
-	// End of user code block custom block entities
+
 	private static <T extends BlockEntity> DeferredHolder<BlockEntityType<?>, BlockEntityType<T>> register(String registryname, DeferredHolder<Block, Block> block, BlockEntityType.BlockEntitySupplier<T> supplier) {
 		return REGISTRY.register(registryname, () -> BlockEntityType.Builder.of(supplier, block.get()).build(null));
 	}
@@ -34,4 +31,5 @@ public class PlacerModBlockEntities {
 	public static void registerCapabilities(RegisterCapabilitiesEvent event) {
 		event.registerBlockEntity(Capabilities.ItemHandler.BLOCK, PLACER.get(), SidedInvWrapper::new);
 	}
+
 }
